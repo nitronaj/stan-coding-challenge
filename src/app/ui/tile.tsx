2793a3ae390021dbf251fcb08bf46a41cf93e1e4
Charlie Nader <nitro.naj@gmail.com>
2023-11-07 17:21:35 +1100

    style tweak

--- src/app/ui/tile.tsx
+++ src/app/ui/tile.tsx
@@ -23,6 +23,7 @@ const Tile: React.FC<TileProps> = ({ type, name, link, query, background: { url,
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
             backgroundSize: size,
+            backgroundColor: 'transparent',
           }}
         >
           {type && <span className="ml-2 text-white text-xl ">{type}</span>}
